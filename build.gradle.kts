3e1dd6da2b5b0631d95098a7dbf88d5799e7d041
Axel Joly <azn9.pro@gmail.com>
2024-03-20 22:06:36 -0400

    Testing user-installed applications

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,13 @@ group = "dev.azn9"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.discord4j:discord4j-core")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/dev/azn9/Main.java
+++ src/main/java/dev/azn9/Main.java
@@ -1,9 +1,72 @@
 package dev.azn9;
 
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandContexts;
+import discord4j.core.object.command.ApplicationIntegrationType;
+import discord4j.discordjson.json.ApplicationCommandRequest;
+import discord4j.discordjson.json.ApplicationInfoData;
+import discord4j.gateway.intent.IntentSet;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GatewayDiscordClient client = DiscordClientBuilder.create(args[0])
+                .build()
+                .gateway()
+                .setEnabledIntents(IntentSet.nonPrivileged())
+                .login()
+                .block();
+
+        if (client == null) {
+            System.exit(1);
+            return;
+        }
+
+        // Register commands
+        var applicationService = client.rest().getApplicationService();
+
+        applicationService.getCurrentApplicationInfo().map(ApplicationInfoData::id).flatMap(appId -> {
+            return Mono.when(applicationService.bulkOverwriteGlobalApplicationCommand(appId.asLong(), List.of(
+                    ApplicationCommandRequest.builder()
+                            .name("whereami")
+                            .description("Get information about the current context")
+                            .contexts(List.of(ApplicationCommandContexts.BOT_DM.getValue()))
+                            .integrationTypes(List.of(ApplicationIntegrationType.USER_INSTALL.getValue()))
+                            .build(),
+                    ApplicationCommandRequest.builder()
+                            .name("onlyonguildsbyusers")
+                            .description("Get information about the current context")
+                            .contexts(List.of(ApplicationCommandContexts.GUILD.getValue()))
+                            .integrationTypes(List.of(ApplicationIntegrationType.USER_INSTALL.getValue()))
+                            .build(),
+                    ApplicationCommandRequest.builder()
+                            .name("onlyonbotdm")
+                            .description("Get information about the current context")
+                            .contexts(List.of(ApplicationCommandContexts.BOT_DM.getValue()))
+                            .integrationTypes(List.of(ApplicationIntegrationType.USER_INSTALL.getValue()))
+                            .build(),
+                    ApplicationCommandRequest.builder()
+                            .name("onlyongroupdm")
+                            .description("Get information about the current context")
+                            .contexts(List.of(ApplicationCommandContexts.PRIVATE_CHANNEL.getValue()))
+                            .integrationTypes(List.of(ApplicationIntegrationType.USER_INSTALL.getValue()))
+                            .build()
+            )));
+        }).block();
+
+        client.on(ChatInputInteractionEvent.class, event -> {
+            String context = event.getInteraction().getContext().map(ctx -> "`" + ctx.name() + "`").orElse("`No context`");
+            String integrationOwners = event.getInteraction().getAuthorizingIntegrationOwners().toString();
+
+            return event.reply("You are in " + context + " and the integration owners are " + integrationOwners).withEphemeral(true);
+        }).subscribe();
+
+        client.onDisconnect().block();
     }
 
 }
