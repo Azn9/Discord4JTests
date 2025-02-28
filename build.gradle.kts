d5bde2b7515a66d17c17f2d60c700f3faa38c6ff
Axel Joly <azn9.pro@gmail.com>
2025-02-28 23:31:06 +0100

    feat: default project files

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.discord4j:discord4j-core:3.3.0-SNAPSHOT")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.23.1")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 tasks.test {
--- src/main/java/dev/azn9/Main.java
+++ src/main/java/dev/azn9/Main.java
@@ -1,9 +1,33 @@
 package dev.azn9;
 
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.gateway.intent.IntentSet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GatewayDiscordClient client = DiscordClientBuilder.create(args[0])
+                .build()
+                .gateway()
+                .setEnabledIntents(IntentSet.all())
+                .login()
+                .block();
+
+        if (client == null) {
+            Main.LOGGER.error("Failed to login to Discord.");
+            return;
+        }
+
+        Main.LOGGER.info("Successfully logged in to Discord.");
+
+        // TODO
+
+        client.onDisconnect().block();
     }
 
 }
