ac5e6b9a755841db14e272da65718e297b8d8cc7
Axel Joly <azn9.pro@gmail.com>
2025-04-20 12:23:28 +0200

    fix: add missing log

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("com.discord4j:discord4j-core")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
